899a7021af30a95943f6c6543f62b4df6ca4ca07
Clay Heaton <ccheaton@gmail.com>
2018-09-03 10:22:00 -0400

    rename var that was misnamed

--- MainScene.gd
+++ MainScene.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var right_scene
+var left_scene
 
 var shapes = []
 
@@ -16,22 +16,21 @@ var shape_to_replace = 0
 func _ready():
     get_tree().debug_collisions_hint = true
     
-    right_scene = load("res://LeftArea2D.tscn")
+    left_scene = load("res://LeftArea2D.tscn")
     
     # Add initial shapes
     for p in positions:
-        var s = right_scene.instance()
+        var s = left_scene.instance()
         s.position = p
         shapes.append(s)
         add_child(s)
         
     $Timer.start()
 
-
 func replace_shape():
     var old_shape = shapes[shape_to_replace]
     
-    var new_shape = right_scene.instance()
+    var new_shape = left_scene.instance()
     new_shape.position = positions[shape_to_replace]
     shapes[shape_to_replace] = new_shape
     add_child(new_shape)
