464c2da2b1bb621c3b815821f0d930f6e5174c33
Clay Heaton <ccheaton@gmail.com>
2018-09-03 11:48:41 -0400

    problem fixed

--- LeftArea2D.gd
+++ LeftArea2D.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 var textures = ["image1.png", "image2.png", "image3.png", "image4.png"]
 
+var collision_shape
+
 func _ready():
     get_tree().debug_collisions_hint = true
     
@@ -15,8 +17,14 @@ func _ready():
     var sprite_height = $LeftSprite.texture.get_height()
     var dimensions = Vector2(sprite_width / 2, sprite_height / 2)
     
+    collision_shape = CollisionShape2D.new()
+    collision_shape.name = "LeftCollisionShape2D"
+    add_child(collision_shape)
+    
+    collision_shape.shape = RectangleShape2D.new()
+    
     # Set the collision shape extents to match the size of the sprite
-    $LeftCollisionShape2D.shape.set_extents(dimensions)
+    collision_shape.shape.set_extents(dimensions)
     
     $RightArea2D.position.x += 100
     
--- RightArea2D.gd
+++ RightArea2D.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var collision_shape
+
 var textures = ["image1.png", "image2.png", "image3.png", "image4.png"]
 
 func _ready():
@@ -15,5 +17,11 @@ func _ready():
     var sprite_height = $RightSprite.texture.get_height()
     var dimensions = Vector2(sprite_width / 2, sprite_height / 2)
     
+    collision_shape = CollisionShape2D.new()
+    collision_shape.name = "RightCollisionShape2D"
+    add_child(collision_shape)
+    
+    collision_shape.shape = RectangleShape2D.new()
+    
     # Set the collision shape extents to match the size of the sprite
-    $RightCollisionShape2D.shape.set_extents(dimensions)
+    collision_shape.shape.set_extents(dimensions)
